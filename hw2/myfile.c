19f850697afca2218738212ec9f6a248c847ffce
live2skull <live2skull@gmail.com>
2020-05-10 15:01:55 +0900

    1차 작성 완료 - 통계기능 추가 예정

--- hw2/myfile.c
+++ hw2/myfile.c
@@ -1,131 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <pthread.h>
-#include <sys/time.h>
-#include <errno.h>
 
 #define TRUE 1
 #define FALSE 0
 
-
-/* 멀티스레딩 이론
-스레드별로 동일한 객체를 접근할 때 CPU의 원리 (메모리가 아닌 레지스터에서 연산)
-으로 인해 데이터의 atomic이 보장되지 않습니다.
-이에 특정 데이터의 읽기,쓰기를 하나의 스레드에서만 하도록 mutex를 사용합니다.
-
-pthread_cond_wait - cond 객체가 signal을 받을 때까지 실행을 일시정지합니다.
-pthread_cond_signal - cond 객체에 signal을 보냅니다.
-
-*/
-
-
-// TODO - 스레드 동작 확인 / 값 확인 / 알파벳별 갯수 확인
-
-typedef struct sharedobject 
+typedef struct sharedobject
 {
-    FILE *rfile; // 파일 읽기전용 객체
-    int linenum;
-    char* line;
+    FILE *rfile;
     pthread_mutex_t lock;
-    pthread_cond_t cond;
-    int full; // 버퍼에 데이터가 있는가? (1 / 0 구분)
-    int finished; // 파일을 전부 읽었는가?
-    // TODO: 문자열별 통계 데이터 추가
-} so_t;           
+    pthread_cond_t cond_prod; // procedure_producer condition
+    pthread_cond_t cond_cons; // procedure_consumer condition
 
+    char* buf; // 읽은 버퍼 데이터입니다.
+    int linenum; // 읽어준 줄 번호입니다. 1부터 시작합니다.
+    int full; // 버퍼 데이터 준비 여부
+    int eof; // 데이터를 다 읽었는지 여부
+} so_t;
 
-// 파일을 읽고, 버퍼에 저장합니다.
-void *procedure_producer(void *arg) // void: 자료형을 정하지 않음 (알수없음)
+void *procedure_producer(void *arg)
 {
-    so_t *so = arg; // 타입 캐스팅
-    int *ret = malloc(sizeof(int)); // pthread 반환값
-    
-    FILE *rfile = so->rfile;
-    int line_count = 0;
-    
-    char *buf = NULL; // rfile에서 읽은 데이터의 공간을 가리키게 됨.
-    size_t len = 0; // getdelim 의 버퍼크기
-    ssize_t read = 0; // getdelim() 호출시마다 읽은 값
-    
+    so_t *so = arg;
+    unsigned int self = (unsigned int)pthread_self();
+    int *pthread_ret = malloc(sizeof(int)); // 스택 영역에 있다면 밀릴 수 있다.
+
+    char* buf = NULL;
+    ssize_t read = 0; // getdelim() 에 의해 읽힌 데이터 크기
+    size_t length = 0; // getdelim() 버퍼 크기
+
+    printf("prod_%u | init\n", self);
     while (TRUE)
     {
-        // 뮤텍스를 잠급니다. - consumer 에서 다 읽을때까지 기다립니다.
-        pthread_mutex_lock(&so->lock);       
-
-        // delimeter 전까지 rfile에서 읽는다. 
-        read = getdelim(&buf, &len, '\n', rfile);
-        if (read == -1)
+        pthread_mutex_lock(&so->lock);
+        
+        // 다른 스레드에서 파일을 전부 읽고, 버퍼에 남은 데이터가 없음.
+        if (so->full == FALSE, so->eof == TRUE)
         {
-            // 파일의 끝에 다다랐습니다. 더 읽을 수 없습니다.
-            so->full = TRUE;
-            so->finished = TRUE;
-            // 다 읽었음을 알리고 러프를 종료합니다.
-            pthread_cond_signal(&so->cond);
+            // 작업이 종료되었습니다.
+            printf("prod_%u | 스레드 종료\n", self);
             pthread_mutex_unlock(&so->lock);
             break;
         }
-        // 버퍼를 다 읽을때까지 대기합니다.
-        // 아직 버퍼를 consumer에서 처리하지 않았음.
-        while (so->full == TRUE) 
+
+        while (so->full)
         {
-            pthread_cond_wait(&so->cond, &so->lock);
+            // 파일에 끝에 다다랐지만, 버퍼가 차 있으므로 모든 스레드를 동작합니다.
+            if (so->eof)
+            {
+                    pthread_cond_signal(&so->cond_cons);
+                    pthread_cond_signal(&so->cond_prod);
+                    break;
+            }
+            // 신호를 잡을 때까지 대기
+            pthread_cond_wait(&so->cond_prod,&so->lock);
         }
+
+        // 데이터를 읽고 저장한 후 consumer에 신호를 보내고, 뮤텍스를 해제합니다.
+        read = getdelim(&buf, &length, '\n', so->rfile);
+        if (read == -1)
+        {
+            // 파일의 끝을 만났습니다.
+            so->eof = TRUE;
+            so->buf = NULL;
+            pthread_cond_signal(&so->cond_cons);
+            pthread_mutex_unlock(&so->lock);
+            break;
+        }
+        so->linenum++;
+        so->buf = strndup(buf, read);
         so->full = TRUE;
-        so->linenum = line_count;
-        so->line = strndup(buf, read); // 읽은 데이터를 공유합니다.
-        line_count++;
-        pthread_cond_signal(&so->cond);
+        pthread_cond_signal(&so->cond_cons);
         pthread_mutex_unlock(&so->lock);
     }
-    free(buf); // getdelim 에서 동적 할당하였으므로 해제.
-    *ret = line_count; // 읽은 줄 수를 저장하고 스레드를 종료합니다.
-    pthread_exit(ret);
 }
 
 
-// 버퍼에서 자료를 가져옵니다.
 void *procedure_consumer(void *arg)
 {
-    so_t *so = arg; // 타입 캐스팅
-    int *ret = malloc(sizeof(int)); // pthread 반환값
-    
-    FILE *rfile = so->rfile;
-    int line_count = 0; // 읽은 줄 갯수
-    int char_count = 0; // 읽은 문자열 갯수
+    so_t *so = arg;
+    unsigned int self = (unsigned int)pthread_self();
+    int *pthread_ret = malloc(sizeof(int)); // 스택 영역에 있다면 밀릴 수 있다.
 
-    char *buf = NULL; // rfile에서 읽은 데이터의 공간을 가리키게 됨.
-    size_t len = 0; // getdelim 의 버퍼크기
-    ssize_t read = 0; // getdelim() 호출시마다 읽은 값
+    int linenum = 0; // 현재 스레드에서 읽은 파일
 
+    char* buf = NULL;
+    ssize_t read = 0; // getdelim() 에 의해 읽힌 데이터 크기
+    size_t length = 0; // getdelim() 버퍼 크기
+
+    printf("cond_%u | init\n", self);
     while (TRUE)
     {
-        // 뮤텍스를 잠급니다.
-        pthread_mutex_lock(&so->lock); 
-        // 버퍼를 아직 가져오지 못했다면, 버퍼가 찰 때까지 대기합니다.
+        pthread_mutex_lock(&so->lock);
+        if (so->full == FALSE && so->eof)
+        {
+            printf("cond_%u | 스레드 종료\n", self);
+            pthread_mutex_unlock(&so->lock);
+            break;
+        }
+        
         while(so->full == FALSE)
         {
-            pthread_cond_wait(&so->cond, &so->lock);
+            if (so->eof)
+            {
+                // 파일의 끝에 다다랐음. 스레드를 종료합니다.
+                pthread_cond_signal(&so->cond_prod);
+                pthread_cond_signal(&so->cond_cons);
+                break;
+            }
+            pthread_cond_wait(&so->cond_cons, &so->lock);
         }
-        buf = so->line; // 읽은 데이터를 가져옵니다.
-        line_count++;
-        if (so->finished)
-        {
-            so->full = FALSE;
-            pthread_cond_signal(&so->cond);
+        buf = so->buf;
+        if (buf == NULL) 
+        { 
+            pthread_cond_signal(&so->cond_prod);
             pthread_mutex_unlock(&so->lock);
+            break;
         }
-        char_count = strlen(buf); // 없어도 무관 - TODO: 알파벳별 추가
-        line_count++;
-
-        free(so->line); // getdelimeter 동적할당 버퍼 해제
-        pthread_cond_signal(&so->cond);
+        printf("cond_%u | 데이터 읽음 : %s\n", self, buf);
+        linenum++;
+        so->full = FALSE;
+        pthread_cond_signal(&so->cond_prod);
         pthread_mutex_unlock(&so->lock);
     }
-    *ret = line_count;
-    pthread_exit(ret);
+    *pthread_ret = linenum;
+    pthread_exit(pthread_ret);
 }
-
 
 void print_usage()
 {
@@ -133,46 +134,54 @@ void print_usage()
     printf("usage : word_count read_file_path consumer_count producer_count\n");
 }
 
+
 int main(int argc, char* argv[])
 {
-    if (argc != 4)
-    {
-        print_usage(); exit(-1);
-    }
+        if (argc != 4)
+        {
+            print_usage(); exit(-1);
+        }
 
-    char* read_file_path = argv[1];
-    int consumer_count = atoi(argv[2]);
-    int producer_count = atoi(argv[3]);
+        FILE *rfile;
+        rfile = fopen((char*)argv[1], "rb");
 
-    if (consumer_count == 0 || producer_count == 0)
-    {
-        print_usage(); exit(-1);
-    }
+        int amount_prods = atoi(argv[2]);
+        int amount_conds = atoi(argv[3]);
 
-    // 파일을 읽기 모드로 엽니다.
-    FILE* rfile;
-    rfile = fopen((char*)read_file_path, "r");
-    if (rfile == NULL)
-    {
-        perror("파일을 열 수 없습니다.");
-        exit(-1);
-    }
+        so_t *share = malloc(sizeof(so_t));
+        memset(share, 0, sizeof(so_t)); // 객체 메모리 초기화
 
-    // share 오브젝트를 생성합니다.
-    // 모든 producer, consumer 스레드에서 공유하게 됩니다.
-    so_t *share = malloc(sizeof(so_t));
-    share->rfile = rfile;
-    share->line = NULL;
-    share->full = FALSE;
-    share->finished = FALSE;
+        share->rfile = rfile;
+        share->linenum = 0;
+        share->full = FALSE;
+        share->eof = FALSE;
+        pthread_mutex_init(&share->lock, NULL);
+        pthread_cond_init(&share->cond_prod, NULL);
+        pthread_cond_init(&share->cond_cons, NULL);
+        
+        pthread_t prod[100];
+        pthread_t cons[100];
 
-    // critical section 에서 사용될 객체를 생성합니다.
-    // 구조체포인터-> 객체 | pthread_..._init에서 포인터받으므로 & 붙여야합니다.
-    pthread_cond_init(&share->cond, NULL); // NULL : 기본 설정 사용
-    pthread_mutex_init(&share->lock, NULL);
-    
-    printf("pthread mutex / condition initialized");
+        for (int i = 0 ; i < amount_prods ; i++)
+                pthread_create(&prod[i], NULL, procedure_producer, share);
+        for (int i = 0 ; i < amount_conds ; i++)
+                pthread_create(&cons[i], NULL, procedure_consumer, share);
 
-    // 사용된 공유 객체를 해제합니다.
-    free(share);
+        int pthread_exitcode;
+        int* pthread_return;
+
+        for (int i = 0 ; i < amount_conds ; i++) {
+                pthread_exitcode = pthread_join(cons[i], (void **) &pthread_return);
+                // printf("main: consumer_%d joined with %d\n", i, *ret);
+
+        }
+        for (int i = 0 ; i < amount_prods ; i++) {
+                pthread_exitcode = pthread_join(prod[i], (void **) &pthread_return);
+                // printf("main: producer_%d joined with %d\n", i, *ret);
+        }
+
+        free(share);
+        fclose(rfile);
+        return 0;
 }
+
